edcd4270f90db91a709f5e452d2b852ab1e8d7a0
Hidayat arghandabi <hidayatarg@gmail.com>
2019-06-08 19:09:44 +0300

    Redux Connection From MusicsPage Component

--- src/components/pages/MusicsPage.js
+++ src/components/pages/MusicsPage.js
@@ -1,11 +1,29 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
 
-export default class MusicsPage extends Component {
+class MusicsPage extends Component {
+// we need
+    static propTypes = {
+        musics: PropTypes.array.isRequired
+    }
+
+
     render() {
+        console.log('Props: ', this.props);
         return (
             <div>
                 <h2>Musics Page</h2>
             </div>
         )
     }
+    }
+
+// read State
+const mapStateToProps = ({ musics }) => {
+    return {
+        musics
+    }
 }
+
+export default connect(mapStateToProps) (MusicsPage)

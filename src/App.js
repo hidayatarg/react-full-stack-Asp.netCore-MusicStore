5369c2905d4accaaa95ddd7d62d6a81307323c24
Hidayat arghandabi <hidayatarg@gmail.com>
2019-06-09 09:29:00 +0300

    Implemented Footer Component

--- src/App.js
+++ src/App.js
@@ -4,10 +4,10 @@ import MusicsPage from './components/pages/MusicsPage';
 import { Link, Route } from 'react-router-dom';
 
 import 'semantic-ui-css/semantic.min.css';
-import {
-  Container, Divider, Grid, Header, Image, List, Menu, Segment, Visibility,
-} from 'semantic-ui-react';
+import { Container, Image, Menu, Visibility } from 'semantic-ui-react';
 import { menuStyle, fixedMenuStyle } from './helpers/styleHelpers';
+
+import Footer from './components/Footer';
 
 class App extends Component {
   state = {
@@ -64,57 +64,7 @@ class App extends Component {
           Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae, ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra. Vestibulum erat wisi, condimentum sed, commodo vitae, ornare sit amet, wisi. Aenean fermentum, elit eget tincidunt condimentum, eros ipsum rutrum orci, sagittis tempus lacus enim ac dui. Donec non enim in turpis pulvinar facilisis. Ut felis. Praesent dapibus, neque id cursus faucibus, tortor neque egestas augue, eu vulputate magna eros eu erat. Aliquam erat volutpat. Nam dui mi, tincidunt quis, accumsan porttitor, facilisis luctus, metus
         </Container>
 
-        <Segment
-          inverted
-          style={{ margin: '5em 0em 0em', padding: '5em 0em' }}
-          vertical
-        >
-          <Container textAlign='center'>
-            <Grid columns={4} divided stackable inverted>
-              <Grid.Row>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Group 1' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Group 2' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Group 3' />
-                  <List link inverted>
-                    <List.Item as='a'>Link One</List.Item>
-                    <List.Item as='a'>Link Two</List.Item>
-                    <List.Item as='a'>Link Three</List.Item>
-                    <List.Item as='a'>Link Four</List.Item>
-                  </List>
-                </Grid.Column>
-                <Grid.Column>
-                  <Header inverted as='h4' content='Footer Header' />
-                  <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
-                </Grid.Column>
-              </Grid.Row>
-            </Grid>
-            <Divider inverted section />
-            <Image src='https://react.semantic-ui.com/logo.png' centered size='mini' />
-            <List horizontal inverted divided link>
-              <List.Item as='a' href='#'>Site Map</List.Item>
-              <List.Item as='a' href='#'>Contact Us</List.Item>
-              <List.Item as='a' href='#'>Terms and Conditions</List.Item>
-              <List.Item as='a' href='#'>Privacy Policy</List.Item>
-            </List>
-          </Container>
-        </Segment>
+        <Footer/>
 
 
       </div>
---
+++ src/components/Footer.js
@@ -0,0 +1,66 @@
+// stateless component
+import React from 'react';
+import {
+    Container, Divider, Grid, Header, Image, List, Segment,
+} from 'semantic-ui-react';
+
+const Footer = (props) => {
+    return (
+        <div>
+            <Segment
+                inverted
+                style={{ margin: '5em 0em 0em', padding: '5em 0em' }}
+                vertical
+            >
+                <Container textAlign='center'>
+                    <Grid columns={4} divided stackable inverted>
+                        <Grid.Row>
+                            <Grid.Column>
+                                <Header inverted as='h4' content='Group 1' />
+                                <List link inverted>
+                                    <List.Item as='a'>Link One</List.Item>
+                                    <List.Item as='a'>Link Two</List.Item>
+                                    <List.Item as='a'>Link Three</List.Item>
+                                    <List.Item as='a'>Link Four</List.Item>
+                                </List>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Header inverted as='h4' content='Group 2' />
+                                <List link inverted>
+                                    <List.Item as='a'>Link One</List.Item>
+                                    <List.Item as='a'>Link Two</List.Item>
+                                    <List.Item as='a'>Link Three</List.Item>
+                                    <List.Item as='a'>Link Four</List.Item>
+                                </List>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Header inverted as='h4' content='Group 3' />
+                                <List link inverted>
+                                    <List.Item as='a'>Link One</List.Item>
+                                    <List.Item as='a'>Link Two</List.Item>
+                                    <List.Item as='a'>Link Three</List.Item>
+                                    <List.Item as='a'>Link Four</List.Item>
+                                </List>
+                            </Grid.Column>
+                            <Grid.Column>
+                                <Header inverted as='h4' content='Footer Header' />
+                                <p>Extra space for a call to action inside the footer that could help re-engage users.</p>
+                            </Grid.Column>
+                        </Grid.Row>
+                    </Grid>
+                    <Divider inverted section />
+                    <Image src='https://react.semantic-ui.com/logo.png' centered size='mini' />
+                    <List horizontal inverted divided link>
+                        <List.Item as='a' href='#'>Site Map</List.Item>
+                        <List.Item as='a' href='#'>Contact Us</List.Item>
+                        <List.Item as='a' href='#'>Terms and Conditions</List.Item>
+                        <List.Item as='a' href='#'>Privacy Policy</List.Item>
+                    </List>
+                </Container>
+            </Segment>
+        </div>
+    )
+}
+
+
+export default Footer

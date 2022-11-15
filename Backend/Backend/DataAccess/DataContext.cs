c8b45833e42de616e42722a56ad95b0ff8168c95
Hidayat Arghandabi <hidayatarg@gmail.com>
2022-11-15 11:35:48 +0300

    UpdatedCode

--- Backend/Backend/DataAccess/DataContext.cs
+++ Backend/Backend/DataAccess/DataContext.cs
@@ -16,7 +16,7 @@ namespace Backend.DataAccess
         }
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            optionsBuilder.UseNpgsql("User ID =react;Password=react; Server=116.203.116.183;Port=5432;Database=react-music-store;Integrated Security=true;Pooling=true;");
+            optionsBuilder.UseNpgsql("User ID =react;Password=react; Server=;Port=5432;Database=react-music-store;Integrated Security=true;Pooling=true;");
         }
         // DbSets
         public DbSet<Music> Musics { get; set; }
